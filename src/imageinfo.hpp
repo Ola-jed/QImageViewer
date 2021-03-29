95e81f1f714fe6f689f41392dcfa962b634c2f37
Ola-jed <olabijed@gmail.com>
2021-03-29 16:12:29 +0100

    Updated containers

--- src/imageinfo.hpp
+++ src/imageinfo.hpp
@@ -12,15 +12,15 @@
 #include <QPushButton>
 class ImageInfo : public QDialog
 {
-Q_OBJECT
-public:
-    explicit ImageInfo(QWidget *parent = nullptr,const QImage &img = {},const QString &path = "");
-private:
-    QLabel *imageSize;
-    QLabel *fileSize;
-    QLabel *extension;
-    QLabel *lastModified;
-    QPushButton *okBtn;
+    Q_OBJECT
+    public:
+        explicit ImageInfo(QWidget *parent = nullptr,const QImage &img = {},const QString &path = "");
+    private:
+        QLabel *imageSize;
+        QLabel *fileSize;
+        QLabel *extension;
+        QLabel *lastModified;
+        QPushButton *okBtn;
 };
 
 #endif // IMAGEINFO_HPP
